266182422a4390fdd683afc2e9e16a7cb10d15b2
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-07-17 07:01:43 +0200

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use Illuminate\Support\Facades\Validator;
+use Inertia\Inertia;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Book::query()->paginate(config('app.page_lines'));
+
+        return Inertia::render('Books/booksIndex', [
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookRequest $request)
+    {
+        Validator::make($request->all(), [
+            'title' => 'required',
+            'author' => 'required',
+        ])->validate();
+
+        Book::create($request->all());
+
+        return redirect()->back()->with('message', 'Book created');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        Validator::make($request->all(), [
+            'title' => 'required',
+            'author' => 'required',
+        ])->validate();
+
+        $book->update($request->all());
+
+        return redirect()->back()->with('message', 'Book updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->back()
+            ->with('message', 'Book deleted');
+    }
+}
---
+++ resources/js/Pages/Books/booksIndex.vue
@@ -0,0 +1,98 @@
+<template>
+    <app-layout>
+
+        <!-- header -->
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Books List
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+
+                    <button @click="openForm()" 
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New Book</button>
+
+                    <!-- table -->
+                    <table class="table-fixed w-full posts-table">
+                        <thead>
+                            <tr class="bg-gray-100">
+                                <th class="px-4 py-2 w-20">No.</th>
+                                <th class="px-4 py-2">Title</th>
+                                <th class="px-4 py-2">Author</th>
+                                <th class="px-4 py-2">Image</th>
+                                <th class="px-4 py-2">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in data.data">
+                                <td class="px-4 py-2 border">{{ item.id }}</td>
+                                <td class="px-4 py-2 border">{{ item.title }}</td>
+                                <td class="px-4 py-2 border">{{ item.author }}</td>
+                                <td class="px-4 py-2 border"><!-- image --></td>
+                                <td class="border px-4 py-2">
+                                    <button @click="openForm(item)" 
+                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                                    <button @click="deleteItem(item)"  
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <pagination :links="data.links"></pagination>
+
+                    <book-form :isOpen="isFormOpen" 
+                               :isEdit="isFormEdit" 
+                               :form="formObject" 
+                               @formsave="saveItem" 
+                               @formclose="closeModal"></book-form>
+                </div>
+            </div>
+        </div>
+
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/layouts/AppLayout.vue';
+    import Pagination from '../../Components/Pagination.vue';
+    import BookForm from '../../Components/Book/form.vue'
+
+    const defaultFormObject = {
+        title: null, author: null, image: null
+    };
+
+    export default {
+        props: ['data'],
+        components: {
+            AppLayout,
+            Pagination,
+            BookForm
+        },
+        data(){
+            return {
+                isFormOpen: false,
+                isFormEdit: false,
+                formObjecz: defaultFormObject
+            }
+        },
+        methods: {
+            saveItem(item){
+                console.log(item);
+            },
+            closeModal(){
+                this.isFormOpen = false;
+            },
+            openForm(item){
+                this.isFormOpen = true;
+                this.isFormEdit = !!item;
+                this.formObject = item ? item : defaultFormObject;
+            },
+            deleteItem(item){
+                console.log('delete ' + item.id);
+            }
+        }
+    }
+</script>
