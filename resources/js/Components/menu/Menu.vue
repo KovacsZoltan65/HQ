4a58e4d964eb28e53db315691e2baff69a2cb05d
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-08-08 08:15:56 +0200

    Menu

---
+++ resources/js/Components/menu/Menu.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import NavLink from '../../Components/NavLink.vue';
+</script>
+<template>
+    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+        <NavLink :href="route('dashboard')" 
+                 :active="route().current('dashboard')">
+            Dashboard
+        </NavLink>
+        <NavLink :href="route('books')" 
+                 :active="route().current('books')">
+            Books
+        </NavLink>
+        <NavLink :href="route('user.index')" 
+                 :active="route().current('user.index')">
+            Users
+        </NavLink>
+
+        <NavLink :href="route('permission.index')" 
+                 :active="route().current('permission.index')">
+            Permission
+        </NavLink>
+
+        <NavLink :href="route('role.index')" 
+                 :active="route().current('role.index')">
+            Role
+        </NavLink>
+
+        <NavLink :href="route('post.index')" 
+                 :active="route().current('post.index')">
+            Post
+        </NavLink>
+    </div>
+</template>
---
+++ resources/js/Components/menu/ResponsiveMenu.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+</script>
+
+<template>
+    <div class="pt-2 pb-3 space-y-1">
+        <ResponsiveNavLink :href="route('dashboard')" 
+                           :active="route().current('dashboard')">
+            Dashboard
+        </ResponsiveNavLink>
+        <ResponsiveNavLink :href="route('books')" 
+                           :active="route().current('books')">
+            Books
+        </ResponsiveNavLink>
+    </div>
+</template>
