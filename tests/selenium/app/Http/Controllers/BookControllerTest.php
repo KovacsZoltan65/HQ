f9b6c28265014afeb590c53870eba080ce63ce4b
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-08-12 15:50:05 +0200

    Books Test

--- tests/selenium/app/Http/Controllers/BookControllerTest.php
+++ tests/selenium/app/Http/Controllers/BookControllerTest.php
@@ -26,26 +26,49 @@ class BookControllerTest extends TestCase {
     
     public function test_getBooks() {
         
+        // ------------
+        // Paraméterek
+        // ------------
+
+        // Táblázat sor / oldal
         $per_page = Config::get('app.per_page');
-        //Log::info('per_page: ' . $per_page);
         
+        // Request osztály példányosítása
         $request = new Request();
-        $request->merge([
+        // Beállítások és szűrők
+        $conf = [];
+        
+        // Könyvek controller példányosítása
+        $bookController = new BookController();
+
+        // ----------------------------------------------
+        // Adatok lekérése az átadott paraméterek alapján
+        // ----------------------------------------------
+
+        // Csak az oldaltörést adom át.
+        $conf = [
             'config' => [
                 'per_page' => $per_page,
             ],
             'filter' => [],
-        ]);
-        
-        //Log::info('$request: ' . print_r($request->all(), true));
-        
-        $bookController = new BookController();
+        ];
+
+        // A paramétertömb átadása a request osztálynak
+        $request->merge($conf);
+
+        // Adatok lekérése a paraméterek alapján.
         $res = $bookController->getBooks($request);
+
+        // Adathalmaz lekérése asszociatív tömb alakban
         $books_data = $res->getData(true);
+
+        // Könyv adathalmaz
         $books = $books_data['books'];
         
+        // Rekordok száma
         $books_count = count($books['data']);
 
+        // A rekordok számának meg kell egyeznie az átadott paraméterrel
         $this->assertEquals($per_page, $books_count, "Elvárt: {$per_page}; Kapott: {$books_count}");
 
     }
