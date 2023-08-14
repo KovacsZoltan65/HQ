37842b2687a62e6dc0a71fca85c348d330e96085
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-08-14 08:44:45 +0200

    Roles

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,7 +7,7 @@ use App\Http\Requests\UpdateBookRequest;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-//use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class BookController extends Controller
