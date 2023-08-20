0e345c9c1942882294ca85745042ae77e2aee246
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-08-20 16:29:52 +0200

    roles

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,17 +7,17 @@ use App\Http\Requests\UpdateBookRequest;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-//use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class BookController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('can:book list', ['only' => ['index', 'show']]);
-        //$this->middleware('can:book create', ['only' => ['create', 'store']]);
-        //$this->middleware('can:book edit', ['only' => ['edit', 'update']]);
-        //$this->middleware('can:book delete', ['only' => ['destroy']]);
+        $this->middleware('can:book list', ['only' => ['index', 'show']]);
+        $this->middleware('can:book create', ['only' => ['create', 'store']]);
+        $this->middleware('can:book edit', ['only' => ['edit', 'update']]);
+        $this->middleware('can:book delete', ['only' => ['destroy']]);
     }
     
     /**
@@ -30,7 +30,14 @@ class BookController extends Controller
         //return Inertia::render('Books/booksIndex', [
         //    'data' => $data
         //]);
-        return Inertia::render('Books/booksIndex');
+        return Inertia::render('Books/booksIndex', [
+            'can' => [
+                'list' => Auth::user()->can('book list'),
+                'create' => Auth::user()->can('book create'),
+                'edit' => Auth::user()->can('book edit'),
+                'delete' => Auth::user()->can('book delete'),
+            ]
+        ]);
     }
 
     public function getBooks(Request $request)
