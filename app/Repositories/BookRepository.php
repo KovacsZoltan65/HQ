1ea43895f72d1cf6d74c5f8ad3e496b1ede88fec
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-09-04 07:43:02 +0200

    Repository

--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -4,3 +4,24 @@ namespace App\Repositories;
 
 use App\Criteria\BookCriteria;
 use App\Models\Book;
+
+/*
+ * Class BookRepositoryEloquent
+ * @package namespace App\Repositories 
+ */
+class BookRepository extends BaseRepository
+{
+    /**
+     * Specify Model class name
+     * @return class
+     */
+    public function model()
+    {
+        return Book::class;
+    }
+    
+    public function boot()
+    {
+        $this->pushCriteria( BookCriteria::class );
+    }
+}
