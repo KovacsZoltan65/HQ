428169a57b99b645b0133d24b346c7fbd9f773fb
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-09-06 14:52:07 +0200

    User repository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Criteria\UserCriteria;
+use App\Models\User;
+use Prettus\Repository\Eloquent\BaseRepository;
+use function app;
+
+/**
+ * Class UserRepositoryRepositoryEloquent.
+ *
+ * @package namespace App\Repositories;
+ */
+class UserRepository extends BaseRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(UserCriteria::class));
+    }
+    
+}
