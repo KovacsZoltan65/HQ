0ca105e82df5da3a96b7d75f0da78324a786a097
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-09-15 14:57:37 +0200

    Subdomains

--- app/Models/Subdomain.php
+++ app/Models/Subdomain.php
@@ -11,5 +11,9 @@ class Subdomain extends Model
     use HasFactory, 
             SoftDeletes;
     
-    protected $fillable = [];
+    protected $fillable = [
+        'subdomain','url','name',
+        'db_host','db_port','db_name','db_user','db_password',
+        'notification','state_id','is_mirror','sso','access_controll_system','last_export',
+    ];
 }
