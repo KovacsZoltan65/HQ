b584c966625d41570aa712dce118bd2c37248469
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-09-19 14:36:28 +0200

    Book seeder

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+//use Database\Factories\BookFactory;
+
+
 use Database\Factories\BookFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class BookSeeder extends Seeder
 {
@@ -12,7 +16,22 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        $fact = new BookFactory();
-        $fact->count(1000)->create();
+        //$fact = new BookFactory();
+        //$fact->count(1000)->create();
+        
+        Storage::deleteDirectory('public');
+        
+        $this->command->warn(PHP_EOL . 'Create books...');
+        
+        //$book = $this->command->withProgressBar(1000, function(){
+        //    $fact = new BookFactory();
+        //    $fact->count(1000)->create();
+        //});
+        
+        $book = $this->command->withProgressBar(1000, fn () => 
+            (new BookFactory())->count(1000)->create()
+        );
+        
+        $this->command->info(PHP_EOL . 'Books created');
     }
 }
