60f3f60b09cd60da555c0130cbac6d08bdd96eba
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-10-17 13:28:12 +0200

    Components

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+
+    defineProps({
+        modelValue: {
+            type: String,
+            default: true,
+        },
+        options: {
+            type: Array,
+            required: true,
+        }
+    });
+
+    defineEmits(['update.modelValue']);
+
+    const input = ref(null);
+
+    onMounted(() => {
+        if(input.value.hasAttribute('autofocus')){
+            input.value.focus();
+        }
+    });
+
+    defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <select>
+        <option 
+            v-for="option in options" 
+            :key="option.id" 
+            :value="option.id"
+        >{{ option.name }}</option>
+    </select>
+</template>
