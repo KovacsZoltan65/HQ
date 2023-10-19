3bbb208e6e4bbb8baba77e5051bc7a48331fafbb
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-10-19 08:14:45 +0200

    AppLayout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -54,7 +54,7 @@
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
+                                <Link :href="route('dashboard')" :title="$t('dashboard')">
                                     <ApplicationMark class="block h-9 w-auto" />
                                 </Link>
                             </div>
