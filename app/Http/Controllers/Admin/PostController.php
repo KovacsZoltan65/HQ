abe793bfb70c95609c83bd01da982b101f049787
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-11-04 18:08:45 +0100

    Post Repository

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,10 +8,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
-class PostController extends Controller
-{
-    public function __construct()
-    {
+class PostController extends Controller {
+
+    private $repository;
+    
+    public function __construct(\App\Interfaces\PostRepositoryInterface $repository) {
+        
+        $this->repository = $repository;
+        
         $this->middleware('can:post list', ['only' => ['index', 'show']]);
         $this->middleware('can:post create', ['only' => ['create', 'store']]);
         $this->middleware('can:post edit', ['only' => ['edit', 'update']]);
@@ -21,67 +25,61 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
+        
         $posts = (new Post)->newQuery();
         $posts->latest();
         $posts = $posts->paginate(100)->onEachSide(2)->appends(request()->query());
 
         return Inertia::render('Admin/Post/Index', [
-            'posts' => $posts,
-            'can' => [
-                'create' => Auth::user()->can('post create'),
-                'edit' => Auth::user()->can('post edit'),
-                'delete' => Auth::user()->can('post delete'),
-            ],
+                'posts' => $posts,
+                'can' => [
+                    'create' => Auth::user()->can('post create'),
+                    'edit' => Auth::user()->can('post edit'),
+                    'delete' => Auth::user()->can('post delete'),
+                ],
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
+    public function show(string $id) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
+    public function edit(string $id) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
+    public function update(Request $request, string $id) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
+    public function destroy(string $id) {
         //
     }
 }
