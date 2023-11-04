030444ce8950b763ea9d64fbd56d20543a682ddf
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-11-04 15:26:36 +0100

    Napi mentés

--- resources/js/Pages/Admin/Role/roleIndex.vue
+++ resources/js/Pages/Admin/Role/roleIndex.vue
@@ -16,14 +16,9 @@
     ];
 
     const data = [
-        {
-            name:       'Name 01', 
-            position:   '',
-            office:     '',
-            extn:       '',
-            start_date: '',
-            salary:     '',
-        }
+        {name: 'Name 01', position: 'Position 01', office: 'Office 01', extn: 'Extn 01', start_date: '2023-01-01', salary: 'Salary 01',},
+        {name: 'Name 02', position: 'Position 02', office: 'Office 02', extn: 'Extn 02', start_date: '2023-01-01', salary: 'Salary 02',},
+        {name: 'Name 03', position: 'Position 03', office: 'Office 03', extn: 'Extn 03', start_date: '2023-01-01', salary: 'Salary 03',},
     ];
 
     const options = {
@@ -52,14 +47,7 @@
                         :options="options" 
                         :columns="columns"
                         :data="data" 
-                        class="display">
-                        <thead>
-                            <tr>
-                                <th>A</th>
-                                <th>B</th>
-                            </tr>
-                        </thead>
-                    </DataTable>
+                        class="display" />
 
                 </div>
             </div>
